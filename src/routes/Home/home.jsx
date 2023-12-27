7aad2d9fcc5dcd19d18124add2d6d2c51fea33cc
julesperret <30316988+julesperret@users.noreply.github.com>
2023-12-27 17:01:51 -0500

    Initial commit

---
+++ src/routes/Home/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Notes from '../../components/Notes'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Minature Vite + React demo website.</p>
+      <p>It uses NodeJs, Express & MongoDB as a backend!</p>
+
+      <Notes/>
+    </div>
+  );
+}
+
+export default Home
